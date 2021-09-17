c876b6defd8a07fe148e2ba95cae9f68906cb1b4
akakou <akakou571@gmail.com>
2021-09-18 04:35:19 +0900

    [clean] Rename variables

--- src/sign.rs
+++ src/sign.rs
@@ -4,55 +4,55 @@ use crate::Signature;
 use crate::GPK;
 use crate::USK;
 use alloc::vec::Vec;
-use bls12_381::{pairing, G1Projective, G2Projective, Scalar};
-use group::{Curve, Group, GroupEncoding};
+use bls12_381::{pairing, G1Projective, Scalar};
+use group::{Curve, GroupEncoding};
 use rand::RngCore;
 
 pub struct SignCredBeforeHashing {
-    a: Scalar,
-    ra: Scalar,
-    r_delta1: Scalar,
-    t1: G1Projective,
-    delta1: Scalar,
-    r1: G1Projective,
-    r4: G1Projective,
+    y: Scalar,
+    r: Scalar,
+    r_delta: Scalar,
+    t: G1Projective,
+    delta: Scalar,
+    r_first: G1Projective,
+    r_second: G1Projective,
 }
 
 pub struct SignCredAfterHashing {
-    sa: Scalar,
-    s_delta1: Scalar,
+    s: Scalar,
+    s_delta: Scalar,
 }
 
 pub fn process_sign_before_hash(
     x: &Scalar,
     rx: &Scalar,
-    u: &G1Projective,
+    pk: &G1Projective,
     rng: &mut impl RngCore,
 ) -> SignCredBeforeHashing {
-    let a = gen_rand_scalar(rng);
-    let ra = gen_rand_scalar(rng);
-    let r_delta1 = gen_rand_scalar(rng);
-    let t1 = u * a;
-    let delta1 = a * x;
-    let r1 = u * ra;
-    let r4 = t1 * rx + u * -r_delta1;
+    let y = gen_rand_scalar(rng);
+    let r = gen_rand_scalar(rng);
+    let r_delta = gen_rand_scalar(rng);
+    let t = pk * y;
+    let delta = y * x;
+    let r_first = pk * r;
+    let r_second = t * rx + pk * -r_delta;
 
     SignCredBeforeHashing {
-        a,
-        ra,
-        r_delta1,
-        t1,
-        delta1,
-        r1,
-        r4,
+        y,
+        r,
+        r_delta,
+        t,
+        delta,
+        r_first,
+        r_second,
     }
 }
 
-pub fn process_sign_after_hash(a: &SignCredBeforeHashing, c: &Scalar) -> SignCredAfterHashing {
-    let sa = a.ra + c * a.a;
-    let s_delta1 = a.r_delta1 + c * a.delta1;
+pub fn process_sign_after_hash(y: &SignCredBeforeHashing, c: &Scalar) -> SignCredAfterHashing {
+    let s = y.r + c * y.y;
+    let s_delta = y.r_delta + c * y.delta;
 
-    SignCredAfterHashing { sa, s_delta1 }
+    SignCredAfterHashing { s, s_delta }
 }
 
 pub fn sign(usk: &USK, gpk: &GPK, rng: &mut impl RngCore) -> Signature {
@@ -70,23 +70,23 @@ pub fn sign(usk: &USK, gpk: &GPK, rng: &mut impl RngCore) -> Signature {
     let a = process_sign_before_hash(x, &rx, u, rng);
     let b = process_sign_before_hash(x, &rx, v, rng);
 
-    let t3 = usk.a + h * (a.a + b.a);
+    let t3 = usk.a + h * (a.y + b.y);
 
     let a1 = pairing(&t3.to_affine(), &g2.to_affine());
     let a2 = pairing(&h.to_affine(), &w.to_affine());
     let a3 = pairing(&h.to_affine(), &g2.to_affine());
 
-    let r3 = a1 * rx + a2 * (-a.ra - b.ra) + a3 * (-a.r_delta1 - b.r_delta1);
+    let r3 = a1 * rx + a2 * (-a.r - b.r) + a3 * (-a.r_delta - b.r_delta);
 
     let mut c: Vec<u8> = vec![];
-    c.append(&mut a.t1.to_bytes().as_ref().to_vec());
-    c.append(&mut b.t1.to_bytes().as_ref().to_vec());
+    c.append(&mut a.t.to_bytes().as_ref().to_vec());
+    c.append(&mut b.t.to_bytes().as_ref().to_vec());
     c.append(&mut t3.to_bytes().as_ref().to_vec());
-    c.append(&mut a.r1.to_bytes().as_ref().to_vec());
-    c.append(&mut b.r1.to_bytes().as_ref().to_vec());
+    c.append(&mut a.r_first.to_bytes().as_ref().to_vec());
+    c.append(&mut b.r_first.to_bytes().as_ref().to_vec());
     c.append(&mut r3.to_bytes().as_ref().to_vec());
-    c.append(&mut a.r4.to_bytes().as_ref().to_vec());
-    c.append(&mut b.r4.to_bytes().as_ref().to_vec());
+    c.append(&mut a.r_second.to_bytes().as_ref().to_vec());
+    c.append(&mut b.r_second.to_bytes().as_ref().to_vec());
 
     let c = calc_sha256_scalar(&c);
 
@@ -96,14 +96,14 @@ pub fn sign(usk: &USK, gpk: &GPK, rng: &mut impl RngCore) -> Signature {
     let s_b = process_sign_after_hash(&b, &c);
 
     Signature {
-        t1: a.t1,
-        t2: b.t1,
+        t1: a.t,
+        t2: b.t,
         t3,
         c,
-        sa: s_a.sa,
-        sb: s_b.sa,
+        sa: s_a.s,
+        sb: s_b.s,
         sx,
-        s_delta1: s_a.s_delta1,
-        s_delta2: s_b.s_delta1,
+        s_delta1: s_a.s_delta,
+        s_delta2: s_b.s_delta,
     }
 }
